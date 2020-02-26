84a38b20cb2bf5114c85e059e4d9bb6f27192821
Virginia Ladd <virginialadd2@gmail.com>
2020-02-26 15:49:52 -0700

    Add user functionality to app component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -15,18 +15,22 @@ class Login extends Component {
   updateChoice = (e) => {
     e.target.classList.add('active')
   }
+  submitInfo = (props) => {
+    this.props.addUser({ name: this.state.name, team: this.state.team })
+    this.setState({ name: '' })
+  }
   render() {
     return(
       <form className='login-form'>
         <h1 className='title-logo'>Clash of the Nerds</h1>
-        <input className='user-name' placeholder='name' type='text' value={this.state.name} onChange={this.handleChange}/>
+        <input className='user-name' placeholder='name' type='text' value={this.state.name} onChange={this.handleChange} maxlength='25'/>
         <h3>Choose Your Team</h3>
         <select className='form-choice' >
           <option value='mythology' onClick={this.updateChoice}>Mythology</option>
           <option value='science' onClick={this.updateChoice}>Science & Nature</option>
           <option value='art' onClick={this.updateChoice}>Art</option>
         </select>
-        <button type='button' className='submit-btn'>Ready to Rumble</button>
+        <button type='button' className='submit-btn' onClick={this.submitInfo}>Ready to Rumble</button>
       </form>
     )
   }
