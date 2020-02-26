2c584ec2456a9f93876320e2548a8a1e626ac722
Virginia Ladd <virginialadd2@gmail.com>
2020-02-26 15:37:07 -0700

    Create login component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -12,10 +12,21 @@ class Login extends Component {
   handleChange = (event) => {
     this.setState({ name: event.target.value })
   }
+  updateChoice = (e) => {
+    e.target.classList.add('active')
+  }
   render() {
     return(
-      <form>
-        <input placeholder='name' type='text' value={this.state.name} onChange={this.handleChange}/>
+      <form className='login-form'>
+        <h1 className='title-logo'>Clash of the Nerds</h1>
+        <input className='user-name' placeholder='name' type='text' value={this.state.name} onChange={this.handleChange}/>
+        <h3>Choose Your Team</h3>
+        <select className='form-choice' >
+          <option value='mythology' onClick={this.updateChoice}>Mythology</option>
+          <option value='science' onClick={this.updateChoice}>Science & Nature</option>
+          <option value='art' onClick={this.updateChoice}>Art</option>
+        </select>
+        <button type='button' className='submit-btn'>Ready to Rumble</button>
       </form>
     )
   }
