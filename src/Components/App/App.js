84f1fad5eae5da0ba4cfbfca5a57ef50c04b0fe1
Virginia Ladd <virginialadd2@gmail.com>
2020-02-26 18:24:36 -0700

    Update app state to hold trivia data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Login from '../Login/Login';
 import Nerds from '../Nerds/Nerds';
+import LoadingPage from '../LoadingPage/LoadingPage';
+import TestContainer from '../TestContainer/TestContainer';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component{
   constructor() {
@@ -10,19 +13,46 @@ class App extends Component{
       user: {
         name: '',
         team: ''
-      },
-      trivia: []
+      }
     }
   }
   addUser = (user) => {
     this.setState({ user })
   }
+  componentDidMount = () => {
+    fetch('https://opentdb.com/api.php?amount=10&category=20')
+      .then(response => response.json())
+      .then(mythology => {
+        this.setState({ mythology })
+      })
+    fetch('https://opentdb.com/api.php?amount=10&category=17')
+      .then(response => response.json())
+      .then(science => {
+        this.setState({ science })
+      })
+    fetch('https://opentdb.com/api.php?amount=10&category=25')
+      .then(response => response.json())
+      .then(art => {
+        this.setState({ art })
+      })
+  }
   render() {
 
     return (
       <div className='App'>
-      <Login addUser={this.addUser}/>
-      <Nerds />
+      <Switch>
+        <section>
+          <Route exact path='/login' render={() => <Login addUser={this.addUser}/>} />
+          <Route exact path='/login' component={Nerds} />
+        </section>
+        <Route exact path='/test' render={() => {
+          return (
+            <section>
+              {!this.props.trivia.length && <LoadingPage />}
+              {this.props.trivia.length && <TestContainer allQuestions={this.props.trivia} />}
+            </section>
+          )}} />
+      </Switch>
       </div>
     );
   }
