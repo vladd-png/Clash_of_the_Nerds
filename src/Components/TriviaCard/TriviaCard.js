f93660697e391636037b1e7b9ece96c9ff91fabf
Virginia Ladd <virginialadd2@gmail.com>
2020-02-27 11:44:05 -0700

    Send trivia answer to store

--- src/Components/TriviaCard/TriviaCard.js
+++ src/Components/TriviaCard/TriviaCard.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TriviaCard.scss';
+import { connect } from 'react-redux';
+import { addIncorrectGuess, addCorrectGuess } from '../../actions';
+
+
 
 // category: "Mythology"
 // type: "multiple"
@@ -8,17 +12,41 @@ import './TriviaCard.scss';
 // correct_answer: "The Sea"
 // incorrect_answers: (3) ["War", "Sun", "Fire"]
 
-const TriviaCard = (props) => {
-  let allAnswers = props.trivia.incorrect_answers.concat(props.trivia.correct_answer)
-  let formattedAnswers = allAnswers.map(answer => {
-    return <button type='button' className='trivia-choice' key={Math.random()}>{answer}</button>
-  })
-  return (
-    <div className='trivia-card'>
-      <h1>{props.trivia.question}</h1>
+class TriviaCard extends Component{
+  constructor() {
+    super()
+    this.state = {
+      choice: ''
+    }
+  }
+  updateChoice = (event) => {
+    if(event.target.value === this.props.trivia.correct_answer) {
+      this.props.loadCorrectGuessToStore(event.target.value)
+    } else {
+      this.props.loadIncorrectGuessToStore(this.props.trivia)
+      this.props.loadIncorrectGuessToStore(event.target.value)
+    }
+    this.setState({ choice: event.target.value })
+  }
+  submitAnswer = (event) => {
+
+  }
+  render() {
+    let allAnswers = this.props.trivia.incorrect_answers.concat(this.props.trivia.correct_answer)
+    let formattedAnswers = allAnswers.map(answer => {
+      return <button type='button' className='trivia-choice' value={answer} key={Math.random()} onClick={this.updateChoice}>{answer}</button>
+    })
+    return (
+      <div className='trivia-card'>
       <div>{formattedAnswers}</div>
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
-export default TriviaCard;
+export const mapDispatchToProps = dispatch => ({
+  loadCorrectGuessToStore: (guess) => { dispatch(addCorrectGuess(guess)) },
+  loadIncorrectGuessToStore: (guess) => { dispatch(addIncorrectGuess(guess)) }
+})
+
+export default connect(null, mapDispatchToProps)(TriviaCard)
