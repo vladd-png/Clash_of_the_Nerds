def7e258fcd6bfe0160c9c65c8c7f902b0d79ad9
Virginia Ladd <virginialadd2@gmail.com>
2020-02-27 10:04:56 -0700

    Setup trivia card component

--- src/Components/TriviaCard/TriviaCard.js
+++ src/Components/TriviaCard/TriviaCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './TriviaCard.scss';
 
+// category: "Mythology"
+// type: "multiple"
+// difficulty: "easy"
+// question: "The greek god Poseidon was the god of what?"
+// correct_answer: "The Sea"
+// incorrect_answers: (3) ["War", "Sun", "Fire"]
+
 const TriviaCard = (props) => {
-  console.log(props);
+  // console.log(props.trivia);
   return (
-    null
+    <div className='trivia-card'>
+      <h1>{props.trivia.question}</h1>
+      <h1>{props.trivia.correct_answer}</h1>
+      <h1>{props.trivia.incorrect_answers}</h1>
+    </div>
   )
 }
 
