f6652e34ecb6e458a1a6d28a7c09187eafb6e747
Virginia Ladd <virginialadd2@gmail.com>
2020-02-26 18:18:31 -0700

    Create TestContainer component

---
+++ src/Components/TestContainer/TestContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TestContainer.scss';
+
+const TestContainer = () => {
+  return (
+    <section>
+      <article className='test-question'>
+      </article>
+    </section>
+  )
+}
+
+export default TestContainer;
