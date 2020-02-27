33286277c7332eb3a79fbd6826a70465c52ee2cf
Virginia Ladd <virginialadd2@gmail.com>
2020-02-26 21:23:21 -0700

    Map state to props for TestContainer

--- src/Components/TestContainer/TestContainer.js
+++ src/Components/TestContainer/TestContainer.js
@@ -5,10 +5,20 @@ const TestContainer = () => {
   return (
     <section>
       <article className='test-question'>
-        
+        <h1>Hello</h1>
+      </article>
+      <article className='test-choice'>
+        <h1>Hello</h1>
+      </article>
+      <article className='test-progress'>
+        <h1>Hello</h1>
       </article>
     </section>
   )
 }
+
+export const mapStateToProps = state => ({
+  trivia: state.trivia
+})
 
 export default TestContainer;
