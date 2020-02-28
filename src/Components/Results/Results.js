1c820fd58d4e9e4e5ef3f7fc0ec19ba0c13c8461
Virginia Ladd <virginialadd2@gmail.com>
2020-02-27 17:21:00 -0700

    Populate results with correct answers

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -5,9 +5,16 @@ import './Results.scss';
 
 const Results = (props) => {
   const allWrong = props.incorrectGuess.map(guess => {
-    console.log(guess);
     return <TriviaResults result={guess} />
   })
+  const allCorrect = props.correctGuess.map(guess => {
+    if (props.correctGuess.length !== 0) {
+      return <TriviaResults result={guess} />
+    } else {
+      return 'You Need To Study'
+    }
+  })
+  console.log(props);
   const totalScore = (10 - props.correctGuess.length)
   const percentCorrect = ((props.correctGuess.length / 10) * 100)
   return (
@@ -19,8 +26,12 @@ const Results = (props) => {
         <h1>You Missed {totalScore} out of 10</h1>
       </div>
       <div className='wrong-results'>
-        <h1 className='wrong-title'>You Missed The Following</h1>
+        <h1 className='wrong-title'>You Missed</h1>
         {allWrong}
+      </div>
+      <div className='correct-results'>
+        <h1 className='wrong-title'>You Answered Correct</h1>
+        {allCorrect}
       </div>
     </section>
   )
