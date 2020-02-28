11ee636b42fc579d786671b5b2ab48476d57f1aa
Virginia Ladd <virginialadd2@gmail.com>
2020-02-27 22:16:30 -0700

    Nerd snapshot written

---
+++ src/Components/Nerds/Nerds.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nerds from './Nerds';
+import { shallow } from 'enzyme';
+
+describe('Nerds', () => {
+  let wrapper = shallow(<Nerds />)
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+  
+})
