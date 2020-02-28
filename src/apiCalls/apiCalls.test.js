3f6f3b2ba8f64b6d9c59fd77ef70c6f9af26eaac
Virginia Ladd <virginialadd2@gmail.com>
2020-02-27 21:12:27 -0700

    Pass three more apiCalls tests

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -10,8 +10,12 @@ describe('fetchData', () => {
     incorrect_answers: (3) ["Hercules", "Perseus", "Daedalus"],
     userGuess: "Perseus"
   }];
+  let mockTeam;
+  let mockLevel;
 
   beforeEach(() => {
+    mockTeam = '20';
+    mockLevel = 'easy';
     window.fetch = jest.fn().mockImplementation(() => {
       return Promise.resolve({
         ok: true,
@@ -21,10 +25,28 @@ describe('fetchData', () => {
   });
 
   it('should call fetch with the correct url', () => {
-    let mockTeam = '20';
-    let mockLevel = 'easy';
     getTrivia(mockTeam, mockLevel);
     expect(window.fetch).toHaveBeenCalledWith(`https://opentdb.com/api.php?amount=10&category=${mockTeam}&difficulty=${mockLevel}`);
   });
+
+  it('HAPPY: should return an array of trivia objects', () => {
+    expect(getTrivia(mockTeam, mockLevel)).resolves.toEqual(mockResponse)
+  });
+
+  it('SAD: should return an error if ok is false', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false
+      })
+    })
+    expect(getTrivia(mockTeam, mockLevel)).rejects.toEqual(Error('Error while fetching.'))
+  });
+
+  it('SAD: should return an error if Promise rejects', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(Error('Error while fetching.'))
+    })
+    expect(getTrivia(mockTeam, mockLevel)).rejects.toEqual(Error('Error while fetching.'))
+  })
 
 })
