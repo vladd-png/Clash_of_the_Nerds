6b4caaf02f105be107d691755ff1b1b5cb57ff07
Virginia Ladd <virginialadd2@gmail.com>
2020-03-02 16:13:13 -0700

    Update button styling to show what trivia cards have been saved

--- src/Components/TriviaResults/TriviaResults.js
+++ src/Components/TriviaResults/TriviaResults.js
@@ -5,15 +5,17 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 export const TriviaResults = (props) => {
-  const updateStoreAnswers = () => {
+  const updateStoreAnswers = (event) => {
+    event.target.classList.add('chosen-card')
     props.saveUsersTrivia(props.result)
+
   }
   return (
     <div className='results-card'>
       <h2>{props.result.question}</h2>
       <h4>Your Answer: {props.result.userGuess}</h4>
       <h4>Correct Answer: {props.result.correct_answer}</h4>
-      <button type='button' onClick={updateStoreAnswers}>Save Card</button>
+      <button type='button' className='results-btn' onClick={updateStoreAnswers}>Save Card</button>
     </div>
   )
 }
