bcdb07eddee0da06e6107556079291e83d4c27d3
Virginia Ladd <virginialadd2@gmail.com>
2020-03-02 09:13:54 -0700

    Results Comtainer snapshot test update

--- src/Components/Results/Results.test.js
+++ src/Components/Results/Results.test.js
@@ -7,12 +7,45 @@ describe('Results', () => {
 
   beforeEach(() => {
     wrapper = shallow(<Results
-      incorrectGuess={[]}
-      correctGuess={[]}
+      incorrectGuess={[
+        {
+          category: 'Mythology',
+          type: 'multiple',
+          difficulty: 'easy',
+          question: 'The greek god Poseidon was the god of what?',
+          correct_answer: 'The Sea',
+          incorrect_answers: ['War', 'Sun', 'Fire'],
+          userGuess: 'War'
+        }
+      ]}
+      correctGuess={[
+        {
+          category: 'Mythology',
+          type: 'true/false',
+          difficulty: 'easy',
+          question: 'Loki dressed as a woman in norse mythology',
+          correct_answer: 'true',
+          incorrect_answers: ['false'],
+          userGuess: 'true'
+        },
+        {
+          category: 'Mythology',
+          type: 'multiple',
+          difficulty: 'medium',
+          question: 'This Greek mythological figure is the god/goddess of battle strategy (among other things).',
+          correct_answer: 'Athena',
+          incorrect_answers: ['Ares','Artemis','Apollo'],
+          userGuess: 'Athena'
+        }
+      ]}
     />)
   })
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should', () => {
+
   })
 })
